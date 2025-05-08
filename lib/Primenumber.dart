ed3fe197dc8392dfb99041d7d74e2d5eaaa522cb
prajkta <waingankar.prajkta27@gmail.com>
2025-05-09 01:25:08 +0530

    same problem

--- lib/Primenumber.dart
+++ lib/Primenumber.dart
@@ -4,6 +4,4 @@ List<int> nums = [1,2,3,4,5,6];
 int sum = nums.reduce((a,b)=>a+b);
 
 print("sum ofliistelements $sum");
-
-
 }
---
+++ lib/factorial.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+
+void main(){
+
+  print("Enter number");
+  
+  int a = int.parse(stdin.readLineSync()!);
+
+  print("Enter number");
+
+  int b =int.parse(stdin.readLineSync()!);
+
+  int sum = a + b;
+
+  print(sum);
+  
+
+
+
+
+
+
+}
