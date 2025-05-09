86f05178a01f0797023a61815e7a20efce44b2dc
prajkta <waingankar.prajkta27@gmail.com>
2025-05-10 02:01:43 +0530

    sets in dart

---
+++ lib/sets.dart
@@ -0,0 +1,33 @@
+
+import 'dart:async';
+
+class Test{
+
+add(){
+  Set items= {1,2,3,4,5,6};
+  Set itemsNew1 ={6,8,9,4};
+  items.addAll(itemsNew1);
+  print(items);
+  
+  items.add(9);
+  print(items.isEmpty);
+  items.remove(1);
+  print(items);
+  print(items.first);
+  print(items.length);
+  print(items.contains(5));
+  print(items.elementAt(3));
+
+
+
+}
+
+}
+
+void main(){
+
+Test obj=Test();
+obj.add();
+
+
+}
